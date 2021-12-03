48e1227516bcf75e715c293368987e4abb8e7bdd
Aleksey Rodionov <rodial.pro@gmail.com>
2021-12-03 10:24:15 +0400

    Скелет и стартовая страница

---
+++ src/components/DashboardLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex h-screen bg-gray-200 font-roboto">
+
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <Header />
+
+      <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+        <div class="container mx-auto mt-10 px-6 py-8 text-gray-800 ">
+          <slot />
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Header from "./Header.vue";
+</script>
---
+++ src/router.ts
@@ -0,0 +1,18 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+
+import Home from "./views/Home.vue";
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: routes,
+});
+
+export default router;
